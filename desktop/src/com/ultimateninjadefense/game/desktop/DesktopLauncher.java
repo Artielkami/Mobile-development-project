ec5fc0f5ee59a15369f916c73bad61e06f26344b
Khiem <ngockhiem27@gmail.com>
2016-01-29 08:00:03 +0700

    init

---
+++ desktop/src/com/ultimateninjadefense/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ultimateninjadefense.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ultimateninjadefense.game.UNDGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new UNDGame(), config);
+	}
+}
